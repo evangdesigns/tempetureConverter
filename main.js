34034a14c29f86d42e434e5bcaaa630fafe6114b
Evan Grabenstein <evangdesigns@gmail.com>
2019-09-23 00:15:24 -0500

    WIP not taking form value

--- main.js
+++ main.js
@@ -1,21 +1,58 @@
 console.log('Temperture Converter');
+let temp = document.getElementById('temp').value
 
-const toCelsius =  () => {
+const toCelsius = (temp) => {
+  let adjustment = (temp - 32) * (5 / 9);
+  // let converted = adjustment * (5 / 9);
+  converted = Math.round(adjustment * 100) / 100;
+  document.getElementById('tempResult').innerHTML = converted;
+  if (converted > 32) {
+    document.getElementById('tempResult').className = "hot";
+  } else if (converted < 0) {
+    document.getElementById('tempResult').className = "cold";
+  } else {
+    document.getElementById('tempResult').className = "mild";
+  }    
+};
 
-}
+const toFahrenheit = (temp) => {
+  let adjustment = (temp) * (9 / 5) + 32;
+  // let converted = adjustment ;
+  converted = Math.round(adjustment * 100) / 100;
+  document.getElementById('tempResult').innerHTML = converted;
+  if (converted > 90) {
+    document.getElementById('tempResult').className = "hot";
+  } else if (converted < 32) {
+    document.getElementById('tempResult').className = "cold";
+  } else {
+    document.getElementById('tempResult').className = "mild";
+  }    
+};
 
-const toFahrenheit =  () => {
+const convert = (temp) => {
+  let f = document.getElementById('F').checked;
+  let c = document.getElementById('C').checked;
+  if (f) {
+    toFahrenheit(temp);
+  } else if (c) {
+    toCelsius(temp);
+  }
+};
 
-}
+const init = () => {
+  document.getElementById('convertBtn').addEventListener('click', () => {
+    convert(temp)
+  });
+  document.getElementById('convertBtn').addEventListener('keyup', e => {
+    if (e.keyCode == 13) {
+      event.preventDefault();
+      convert(temp);
+    }
+  });
+  document.getElementById('clearBtn').addEventListener('click', () => {
+    document.getElementById('tempResult').innerHTML = "";
+    document.getElementById('temp').value = "";
+  });
+};
 
-// Get a reference to the button element in the DOM
-const button = document.getElementById("converter");
-
-// This function should determine which conversion should
-// happen based on which radio button is selected.
-const determineConverter = (e) => {
-  console.log("event", e);
-}
-
-// Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter);
+init();
